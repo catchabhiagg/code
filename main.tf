fee3d1c81275f3d42abe36fd0eae8ee29c442bab
catchabhiagg <catchabihagg@gmail.com>
2025-08-04 12:51:06 +0530

    Added Terraform files

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "712bfb2b-0ea5-4e2e-ae12-b849c506ebe4"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-demo"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "storage_account_name" {
+  default = "demostorage98761"
+}
